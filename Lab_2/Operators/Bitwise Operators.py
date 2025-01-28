9e046c4c9f75266fb40cb760c518d7c65d14300a
Imangali <imeke.bzb@gmail.com>
2025-01-28 23:06:11 +0500

    lab_2

---
+++ Lab_2/Operators/Bitwise Operators.py
@@ -0,0 +1,11 @@
+""" 
+Operator	Name		            Example	
+
+& 	        AND		                x & y	
+|	        OR		                x | y	
+^	        XOR		                x ^ y	
+~	        NOT		                ~x	
+<<	        Zero fill left shift	x << 2	
+>>	        Signed right shift		x >> 2
+
+"""
